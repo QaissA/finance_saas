b110849af85598e08993140aafab6de0a4e534b1
QaissA <qaissabdelhamid@gmail.com>
2024-06-23 18:44:04 +0200

    init commit

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,19 @@
+import Header from '@/components/header'
+import React from 'react'
+
+type props = {
+    children: React.ReactNode
+}
+
+const layout = ({ children }: props) => {
+    return (
+        <>
+            <Header />
+            <main className='px-3 lg:px-14'>
+                {children}
+            </main>
+        </>
+    )
+}
+
+export default layout
