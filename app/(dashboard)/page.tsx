6abae55639cab44175dbed11f2a106497d2176a0
QaissA <qaissabdelhamid@gmail.com>
2024-06-30 12:41:38 +0200

    adding post request with the form to add new account

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,8 +1,14 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-accounts"
 
 export default function Home() {
+  const { onOpen } = useNewAccount();
+
   return (
     <div>
-      home Page.
+      <Button onClick={onOpen}>New Account</Button>
     </div>
   )
 }
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -7,13 +7,13 @@ export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app.onError((err, c) => {
-  if (err instanceof HTTPException) {
-    return err.getResponse();
-  }
+// app.onError((err, c) => {
+//   if (err instanceof HTTPException) {
+//     return err.getResponse();
+//   }
 
-  return c.json({ error: "Internal Error" }, 500);
-});
+//   return c.json({ error: "Internal Error" }, 500);
+// });
 
 const routes = app.route("/accounts", accounts);
 
